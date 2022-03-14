7c02ae1e038a769df44af7fca92cfddb52eab314
hosibito <hosibito@naver.com>
2022-03-14 23:45:01 +0900

    2.3 'As' and Attrs 01

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Link = styled(Btn)`
+
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">Log in</Btn>
     </Father>
   );
 }
 
 export default App;
 
-/*
-  css 에 프로퍼티를 줄수 있다. 
-  확장도 가능하다!!
-*/
+/**
+ * 확장뿐 아니라 변경도 가능하다. 
+ * Btn 으로 만들어진 스타일을 링크에도 가져다 쓰고 싶은데. 
+ *   1. Link 를 만들어서 Btn을 상속받는 방법
+ *   2. Btn 을 잠시 a 태그로 바꿔서 써먹는 방법
+ */
+
