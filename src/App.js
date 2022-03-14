2bff620d0b427665c6cec047b6e6d2db9c631ff5
hosibito <hosibito@naver.com>
2022-03-14 23:54:00 +0900

    2.3 'As' and Attrs 02

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true , minLength: 10})`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-const Link = styled(Btn)`
-
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">Log in</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
@@ -27,7 +23,7 @@ function App() {
 export default App;
 
 /**
- * 확장뿐 아니라 변경도 가능하다. 
+ * Attrs 를 직접 추가할수도 있다. 
  * Btn 으로 만들어진 스타일을 링크에도 가져다 쓰고 싶은데. 
  *   1. Link 를 만들어서 Btn을 상속받는 방법
  *   2. Btn 을 잠시 a 태그로 바꿔서 써먹는 방법
